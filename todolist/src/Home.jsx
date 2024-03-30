10a3aff757710754c88732b8fd0f9c696c76e78d
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-03-30 13:22:52 +0530

    checkbox function

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Create from "./Create";
+import { BsFillTrashFill, BsCircleFill } from 'react-icons/bs';
 
 function Home(){
     const[todos,setTodos]=useState([])
@@ -9,6 +10,13 @@ function Home(){
         .then(result => setTodos(result.data))
         .catch(err => console.log(err))
     },[])
+
+    const handleEdit = (id)=>{
+        axios.put('http://localhost:3001/update/'+id)
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
+    }
+
     return(
         <div className="home">
             <h1>Todo List</h1>
@@ -18,8 +26,16 @@ function Home(){
                 ?
                 <div><h2>No Records</h2></div>
                 :
-                todos.map((todo)=>(
-                    <div className="task_list">{todo.task}</div>
+                todos.map((todo,index)=>(
+                    <div className="task_list" key={index}>
+                        <div className="checkbox" onClick={()=>handleEdit(todo._id)}>
+                            <BsCircleFill className="icon"/>
+                            <p>{todo.task}</p>
+                        </div>
+                        <div>
+                            <span><BsFillTrashFill className="icon"/></span>
+                        </div>
+                        </div>
                 ))
             }
         </div>
