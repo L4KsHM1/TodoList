bf9683189a771d40a2101bbe547bd298e53d2539
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-04-03 11:53:21 +0530

    db change

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const mongoose =require('mongoose')
+const mongoose = require('mongoose')
 const cors = require('cors')
 const TodoModel = require('./Models/Todo')
 
@@ -7,35 +7,39 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb://localhost:27017/test')
+// mongoose.connect('mongodb://localhost:27017/test')
 
+app.get('/', (req, res) => {
+    console.log('hello world')
+    res.send('hello world')
+})
 //to display data
-app.get('/get',(req,res)=>{
+app.get('/get', (req, res) => {
     TodoModel.find()
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
+        .then(result => res.json(result))
+        .catch(err => res.json(err))
 })
-app.put('/update/:id',(req,res)=>{
-    const {id} = req.params;
-    TodoModel.findByIdAndUpdate({_id: id}, {done:true})
-    .then(result =>  res.json(result))
-    .catch(err => res.json(err))
-})
-
-app.delete('/delete/:id',(req,res)=>{
-    const {id} = req.params;
-    TodoModel.findByIdAndDelete({_id:id})
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
+app.put('/update/:id', (req, res) => {
+    const { id } = req.params;
+    TodoModel.findByIdAndUpdate({ _id: id }, { done: true })
+        .then(result => res.json(result))
+        .catch(err => res.json(err))
 })
 
-app.post('/add',(req,res) =>{
+app.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    TodoModel.findByIdAndDelete({ _id: id })
+        .then(result => res.json(result))
+        .catch(err => res.json(err))
+})
+
+app.post('/add', (req, res) => {
     const task = req.body.task;
     TodoModel.create({
-        task:task
-    }).then(result=>res.json(result))
-    .catch(err => res.json(err))
+        task: task
+    }).then(result => res.json(result))
+        .catch(err => res.json(err))
 })
-app.listen(3001,()=>{
+app.listen(3001, () => {
     console.log("server is running")
 })
